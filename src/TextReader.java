003d4646c14087b4eae37f4722b0cc77fda9eeac
ReleasedBrainiac <Tobias.Turke@gmx.net>
2016-10-21 22:44:04 +0200

    Add files via upload

---
+++ src/TextReader.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+	
+/**
+ * Simple file Reader for *.txt files.
+ * @author TTurke
+ *
+ */
+public class TextReader 
+{
+	
+	public static String fileReader(String path)
+	{
+		BufferedReader br = null;
+		String str = "";
+
+		try {
+
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader(path));
+			while ((sCurrentLine = br.readLine()) != null) str += sCurrentLine;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return str;
+	}
+}
